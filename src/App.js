bf12e95940ae8f859f78e020b7711c8d8b3335aa
kajeta <ayodejiatanda1@gmail.com>
2022-07-23 20:56:19 +0100

    added operand functionality

--- src/App.js
+++ src/App.js
@@ -13,8 +13,15 @@ export const ACTIONS ={
 function reducer(state, {type, payload}){
   switch(type){
     case ACTIONS.ADD_FIGURE:
+      if( state.overwrite === true){
+        return{
+          ...state,
+          currentOperand: payload.figure,
+          overwrite: false
+        }
+      }
       if (payload.figure === "0" && state.currentOperand === "0"){
-        return state
+        return state;
       }
       if (payload.figure === "." && state.currentOperand.includes(".")) {
         return state
@@ -23,7 +30,102 @@ function reducer(state, {type, payload}){
         ...state,
         currentOperand: `${state.currentOperand || " "}${payload.figure}`
       }
+
+    case ACTIONS.CHOOSE_OPERATION:
+        if (state.currentOperand == null && state.prevOperand == null) {
+          return state
+        }
+        if (state.prevOperand == null) {
+          return{ 
+            ...state,
+            operation: payload.operation,
+            prevOperand: state.currentOperand,
+            currentOperand: null
+          }
+        }
+        if (state.currentOperand == null) {
+          return{
+            ...state,
+            operation: payload.operation,
+
+          }
+        }
+        return{
+          ...state,
+          prevOperand: evaluate(state),
+          currentOperand: null,
+          operation: payload.operation
+        }
+    case ACTIONS.EVALUATE:
+    if(state.operation == null || state.currentOperand == null || state.prevOperand== null){
+      return state
     }
+    return{
+      ...state,
+      prevOperand: null,
+      currentOperand: evaluate(state),
+      operation: null,
+      overwrite: true
+    }
+
+    case ACTIONS.CLEAR:
+      return {}
+
+    case ACTIONS.DEL_FIGURE:
+      if(state.overwrite){
+        return{
+          ...state,
+          currentOperand: null,
+          overwrite: false
+        }
+      }
+      if (state.currentOperand == null) return state
+      if (state.currentOperand.lenght === 1){
+        return{
+          ...state,
+          currentOperand: null
+        }
+      }
+      return{
+        ...state,
+        currentOperand: state.currentOperand.slice(0, -1)
+      } 
+    }
+
+    
+}
+
+function evaluate({currentOperand, prevOperand, operation}){
+    const current = parseFloat(currentOperand)
+    const prev= parseFloat(prevOperand)
+    if (isNaN(prev) || isNaN(current)) return ""
+    let calculation = ""
+    switch (operation) {
+      case '+':
+        calculation = prev + current
+        break;
+      case '-':
+        calculation = prev - current
+        break;
+      case '*':
+        calculation = prev * current
+        break;
+      case "÷":
+        calculation = prev / current
+        break;
+    }
+    return calculation.toString()
+}
+
+const INTERGER_FORMATTER = new Intl.NumberFormat("en-us", {
+  maximumFractionDigits: 0,
+})
+
+function formatOperand(operand){
+  if(operand == null) return
+  const [interger, decimal] = operand.split(".")
+  if (decimal == null) return INTERGER_FORMATTER.format(interger)
+  return `${INTERGER_FORMATTER.format(interger)}.${decimal}`
 }
 
 function App() {
@@ -34,11 +136,11 @@ function App() {
   return (
     <div className="calculator-grid">
     <div className="output">
-      <div className="prev-operand">{prevOperand} {operation}</div>
-      <div className="current-operand">{currentOperand}</div>
+      <div className="prev-operand">{formatOperand (prevOperand)} {operation}</div>
+      <div className="current-operand">{formatOperand (currentOperand)}</div>
     </div>
-    <button className="span-two" onClick={() => dispatch()}>AC</button>
-    <button>DEL</button>
+    <button className="span-two" onClick={() => dispatch({type: ACTIONS.CLEAR})}>AC</button>
+    <button onClick={() => dispatch({type: ACTIONS.DEL_FIGURE})}>DEL</button>
     <OperationBtn operation= "÷" dispatch={dispatch}/>
     <FigureBtn figure= "1" dispatch={dispatch}/>
     <FigureBtn figure= "2" dispatch={dispatch}/>
@@ -54,7 +156,7 @@ function App() {
     <OperationBtn operation= "+" dispatch={dispatch}/>
     <FigureBtn figure= "." dispatch={dispatch}/>
     <FigureBtn figure= "0" dispatch={dispatch}/>
-    <button className="span-two">=</button>
+    <button className="span-two"  onClick={() => dispatch({type: ACTIONS.EVALUATE})}>=</button>
     
 
 
